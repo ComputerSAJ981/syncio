0e75f8ef160b15807312dcac7bbca6e56f514b56
ComputerSAJ981 <computer981saj@gmail.com>
2025-09-29 06:11:30 -0700

    Rate limmiting implimented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,14 @@ import messageRoutes from './routes/message.route.js';
 import path from 'path';
 import connectDB from './lib/db.js';
 import { ENV } from './lib/env.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT;
 app.use(express.json()); // Add this line!
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
